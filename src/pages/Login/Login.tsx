62fd40ea0da3cd5fc51b277d0ad5098c39b18743
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2024-05-06 14:04:45 +0300

    Login component V2.0

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,13 +3,45 @@ import { Button } from '../../components/Button/Button';
 import { Headling } from '../../components/Headling/Headling';
 import { Input } from '../../components/Input/Input';
 import styles from './Login.module.css';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
+import axios, { AxiosError } from 'axios';
+import { PREFIX } from '../../helpers/api';
+
+export type LoginForm = {
+	email: {
+		value: string;
+	}
+	password: {
+		value: string;
+	}
+}
 
 export const Login = () => {
+	const [error, setError] = useState<string | null>(null);
+
+
+	const sendLogin = async (email: string, password: string) => {
+		try {
+			const { data } = await axios.post(`${PREFIX}/auth/login`, {
+				email, 
+				password
+			});
+			console.log(data);
+		} catch (e) {
+			if (e instanceof AxiosError) {
+				setError(e.response?.data.message);
+			}
+		}
+	};  
+
 	const submit = (e: FormEvent) => {
 		e.preventDefault();
-		console.log(e);
+		setError(null);
+		const target = e.target as typeof e.target & LoginForm;
+		const { email, password } = target;
+		sendLogin(email.value, password.value);
 	};
+
 
 
 	return (
@@ -20,14 +52,15 @@ export const Login = () => {
 					<label htmlFor="email">
 						Ваш email
 					</label>
-					<Input id="email" placeholder='Email' />
+					<Input id="email" name='email' placeholder='Email' type='email' />
 				</div>
 				<div className={styles.field}>
 					<label htmlFor="password">
 						Ваш пароль
 					</label>
-					<Input id="password" placeholder='Пароль' />
+					<Input id="password" name='password' placeholder='Пароль' type='password ' />
 				</div>
+				{error && <div className={styles.error}>{error}</div>}
 				<Button appearance="big">Вход</Button>
 			</form>
 			<div className={styles.link}>
